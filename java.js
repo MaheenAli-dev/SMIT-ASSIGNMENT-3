85b103e550b643eb436695ccdd90b9f011c1307a
Maheen_Ali_dev <125376391+MaheenAli-dev@users.noreply.github.com>
2023-04-17 06:54:53 -0700

    Update java.js

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-// Define the movie data
+
 const movies = [
     {
       title: 'Godzilla vs. Kong',
@@ -62,31 +62,27 @@ const movies = [
     }
   ];
   
-  // Define a function to display movie recommendations
   function displayRecommendations() {
-    // Get the user's preferences
+  
     const genre = document.getElementById('genre').value;
     const year = parseInt(document.getElementById('year').value);
     const rating = parseInt(document.getElementById('rating').value);
   
-    // Filter the movie data based on the user's preferences
+  
     let filteredMovies = movies.filter(function(movie) {
       return (movie.genre === genre || genre === 'all') &&
              (movie.year === year || year === 'all') &&
              (movie.rating >= rating || rating === 'all');
     });
   
-    // Create a string of HTML for the movie recommendations
     let recommendationsHTML = '';
     for (let i = 0; i < filteredMovies.length; i++) {
       recommendationsHTML += '<li>' + filteredMovies[i].title + '</li>';
     }
   
-    // Update the recommendations list on the UI
     const recommendationsList = document.getElementById('recommendations');
     recommendationsList.innerHTML = recommendationsHTML;
   }
   
-  // Add an event listener to the submit button to display movie recommendations
   const submitButton = document.getElementById('submit');
   submitButton.addEventListener('click', displayRecommendations);
